444d1edfeb2e9efe5284a139f7420e1dd03cec7f
Tom Striker <toms.baugis@gmail.com>
2024-12-09 12:05:07 +0000

    icons - parse fill instruction

--- src/widgets/global/Icon.vue
+++ src/widgets/global/Icon.vue
@@ -12,7 +12,7 @@
 </script>
 
 <template>
-    <span class="material-symbols-outlined">{{ name }}</span>
+    <span class="icon material-symbols-outlined" :class="{filled}">{{ name }}</span>
 </template>
 
 <style lang="scss">
@@ -23,5 +23,9 @@
         width: 1em;
         stroke: none;
         fill: currentColor;
+
+        &.filled {
+            font-variation-settings: "FILL" 1;
+        }
     }
 </style>
